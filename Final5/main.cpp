1439dc6033c79e6ace91402f7aabcb445b9c46e3
Andrey Styskin <styskin@macbook-air-6.local>
2019-02-23 18:26:05 +0300

    Initial Commit

---
+++ Final5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final5
+//
+//  Created by Andrey Styskin on 23.02.2019.
+//  Copyright © 2019 Andrey Styskin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
